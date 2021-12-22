560e52f4dd7b37e743c4120eda8d3b6222f69638
mahdyaslami <mahdyaslami@gmail.com>
2021-12-22 21:55:49 +0330

    Install vue 3.2.26 and config to use in blade.

---
+++ resources/js/vue.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import LaravelLinks from './Shared/LaravelLinks'
+import LaravelLogo from './Shared/LaravelLogo'
+
+createApp({
+    components: {
+        LaravelLinks,
+        LaravelLogo,
+    }
+}).mount('#app')
